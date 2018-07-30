4371e0bd380f5625ef8aa6310c241e5eb9c6d8b4
Vincentas Zarauskas <34156236+Ragerunner@users.noreply.github.com>
2018-07-30 22:29:06 +0300

    core

---
+++ Rocket.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 1f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+
+    enum State { Alive, dying, Transcending }
+    State state = State.Alive;
+
+    bool collisionDisabled = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        if (Debug.isDebugBuild)
+        { RespondToDebugKeys(); }
+
+    }
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextScene();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; // toogle
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive || !collisionDisabled) { return; }
+        switch (collision.gameObject.tag)
+        {
+
+            case "Friendly":
+                // do nothing
+                print("ok"); // todo remove
+
+                break;
+            case ("Finish"):
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextScene", levelLoadDelay);
+    }
+    private void StartDeathSequence()
+    {
+        state = State.dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // skristi
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+    private void ApplyThrust()
+    {
+        rigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (audioSource.isPlaying == false) // kad nepradetu audio vis is naujo
+        {
+            audioSource.PlayOneShot(mainEngine);
+            mainEngineParticles.Play();
+        }
+
+    }
+    private void RespondToRotateInput()
+    {
+        RespondToThrustInput();
+        rigidbody.freezeRotation = true; // rotacijos rankinis valdymas
+
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidbody.freezeRotation = false; // grazina fizika valdymui
+    }
+}
